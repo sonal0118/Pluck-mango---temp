a5f00ef97e6131f172127fba30b4e4eb5280b87f
sonal0118 <69506555+sonal0118@users.noreply.github.com>
2021-06-29 16:26:23 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,33 @@
+class launcher{
+	constructor(body,anchor)
+	{
+			var options={ bodyA:body,			 
+			pointB:anchor, 
+			stiffness:0.004, 
+			length:2			
+		}		
+		this.bodyA=body
+		this.pointB=anchor
+		this.launcher=Constraint.create(options)
+		World.add(world,this.launcher)
+	}
+
+	//define attach function
+
+	fly()
+	{
+		this.launcher.bodyA=null;
+	}
+
+	display()
+	{
+		if(this.launcher.bodyA)
+		{
+			var pointA=this.bodyA.position;
+			var pointB=this.pointB
+
+			strokeWeight(2);		
+			line(pointA.x,pointA.y,pointB.x,pointB.y);
+		}
+	}
+}
